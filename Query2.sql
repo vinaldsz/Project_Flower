5787ad7bd54c924afa9110c48e885e1c167f5c61
Vinal Dsouza <dsouza.vi@northeastern.edu>
2024-10-22 00:49:30 -0700

    Query2 change

--- Query2.sql
+++ Query2.sql
@@ -4,8 +4,5 @@ FROM Orders
 Where customer_id in (
 SELECT customer_id
 FROM Customer
-<<<<<<< HEAD
-where  lower(first_name) = "enrica" and lower(last_name) = "eckert");
-=======
 where  lower(first_name) = "idell" and lower(last_name) = "gerritsma");
->>>>>>> ba5d891 (Modified diagrams, DDL, insert and query 2,4)
+
